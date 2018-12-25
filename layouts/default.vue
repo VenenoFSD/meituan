261836e3e89ca01f4823d8f32c36300582da221f
VenenoFSD <venenofsd@qq.com>
2018-12-25 13:39:54 +0800

    project init

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>
+      <nuxt></nuxt>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+</style>
